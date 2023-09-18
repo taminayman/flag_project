975bd358e6a2ab6f592367ed226be4662e524764
Noam <noamb0304@gmail.com>
2023-09-18 16:46:44 +0300

    creating game board

--- screen.py
+++ screen.py
@@ -1,69 +1,24 @@
 import pygame
 import consts
 import time
+import soldier
 
 
-screen = pygame.display.set_mode(
-    (consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT))
+
+screen = pygame.display.set_mode((consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT))
 
 
-def init_image(filename, size):
-    img = pygame.image.load(filename)
-    return pygame.transform.scale(img, (consts.SQUARE_LEN * size[0], consts.SQUARE_LEN * size[1]))
+def draw_screen():
+    pygame.display.set_caption("The Flag")
+    screen.fill(consts.SCREEN_COLOR)
+    return screen
 
 
-def draw_grass(x, y):
-    screen.blit(grass_image, (x * consts.SQUARE_LEN, y * consts.SQUARE_LEN))
+def put_soldier_in_place():
+    screen.blit(soldier.create_soldier(consts.SOLDIER_IMG), (0, 0))
+    return screen
 
 
-def draw_flag(x, y):
-    screen.blit(flag_image, (x * consts.SQUARE_LEN, y * consts.SQUARE_LEN))
-
-
-def draw_soldier(soldier):
-    y = soldier[0]
-    x = soldier[1]
-    screen.blit(soldier_image, (x * consts.SQUARE_LEN, y * consts.SQUARE_LEN))
-
-
-def draw_game(board, soldier):
-    screen.fill(consts.BACKGROUND_COLOR)
-    for y in range(len(board)):
-        for x in range(len(board[y])):
-            if board[y][x] == consts.GRASS:
-                draw_grass(x, y)
-            elif board[y][x] == consts.FLAG:
-                draw_flag(x, y)
-
-    draw_soldier(soldier)
-
-
-grass_image = init_image(consts.IMG_GRASS, consts.GRASS_SIZE)
-flag_image = init_image(consts.IMG_FLAG, consts.FLAG_SIZE)
-mine_image = init_image(consts.IMG_MINE, consts.MINE_SIZE)
-soldier_image = init_image(consts.IMG_SOLDIER, consts.SOLDIER_SIZE)
-
-time.sleep(5)
-
-
-# import soldier
-
-#
-#
-# screen = pygame.display.set_mode((consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT))
-#
-#
-# def draw_screen():
-#     pygame.display.set_caption("The Flag")
-#     screen.fill(consts.SCREEN_COLOR)
-#     return screen
-#
-#
-# def put_soldier_in_place():
-#     screen.blit(soldier.create_soldier(consts.SOLDIER_IMG), (0, 0))
-#     return screen
-#
-#
-# draw_screen()
-# put_soldier_in_place()
-# pygame.display.flip()
+draw_screen()
+put_soldier_in_place()
+pygame.display.flip()
--- soldier.py
+++ soldier.py
@@ -1,57 +1,15 @@
-# import pygame
-# import consts
-#
-#
-# def create_soldier(soldier_img):
-#     soldier = pygame.image.load(soldier_img)
-#     sized_soldier = pygame.transform.scale(soldier, (
-#         2 * consts.SQUARE_LENGTH, 4 * consts.SQUARE_LENGTH))
-#
-#     soldier_box = pygame.Surface(
-#             (2 * consts.SQUARE_LENGTH, 4 * consts.SQUARE_LENGTH), )
-#     soldier_box.fill(consts.SCREEN_COLOR)
-#     soldier_box.blit(sized_soldier, (0, 0))
-#
-#     return soldier_box
-import consts
 import pygame
-
-screen = pygame.display.set_mode(
-    (consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT))
+import consts
 
 
-def init_image(filename, size):
-    img = pygame.image.load(filename)
-    return pygame.transform.scale(img, (consts.SQUARE_LEN * size[0], consts.SQUARE_LEN * size[1]))
+def create_soldier(soldier_img):
+    soldier = pygame.image.load(soldier_img)
+    sized_soldier = pygame.transform.scale(soldier, (
+        2 * consts.SQUARE_LENGTH, 4 * consts.SQUARE_LENGTH))
 
+    soldier_box = pygame.Surface(
+            (2 * consts.SQUARE_LENGTH, 4 * consts.SQUARE_LENGTH), )
+    soldier_box.fill(consts.SCREEN_COLOR)
+    soldier_box.blit(sized_soldier, (0, 0))
 
-def draw_grass(x, y):
-    screen.blit(grass_image, (x * consts.SQUARE_LEN, y * consts.SQUARE_LEN))
-
-
-def draw_flag(x, y):
-    screen.blit(flag_image, (x * consts.SQUARE_LEN, y * consts.SQUARE_LEN))
-
-
-def draw_soldier(soldier):
-    y = soldier[0]
-    x = soldier[1]
-    screen.blit(soldier_image, (x * consts.SQUARE_LEN, y * consts.SQUARE_LEN))
-
-
-def draw_game(board, soldier):
-    screen.fill(consts.BACKGROUND_COLOR)
-    for y in range(len(board)):
-        for x in range(len(board[y])):
-            if board[y][x] == consts.GRASS:
-                draw_grass(x, y)
-            elif board[y][x] == consts.FLAG:
-                draw_flag(x, y)
-
-    draw_soldier(soldier)
-
-
-grass_image = init_image(consts.IMG_GRASS, consts.GRASS_SIZE)
-flag_image = init_image(consts.IMG_FLAG, consts.FLAG_SIZE)
-mine_image = init_image(consts.IMG_MINE, consts.MINE_SIZE)
-soldier_image = init_image(consts.IMG_SOLDIER, consts.SOLDIER_SIZE)
+    return soldier_box
